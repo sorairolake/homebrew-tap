d79129400cf1699ace4d6e373268c6c085c736f2
Shun Sakai <sorairolake@protonmail.ch>
2024-08-04 20:02:05 +0900

    Update `hf.rb` (#11)

--- Formula/hf.rb
+++ Formula/hf.rb
@@ -5,8 +5,8 @@
 class Hf < Formula
   desc "Utility for hidden file/directory"
   homepage "https://sorairolake.github.io/hf/"
-  url "https://github.com/sorairolake/hf/archive/refs/tags/v0.3.5.tar.gz"
-  sha256 "c6979328056acebdd777885da9540982393c56493807876ae072068da3f70f43"
+  url "https://github.com/sorairolake/hf/archive/refs/tags/v0.3.6.tar.gz"
+  sha256 "0da63f6e255493116f22ba883ce4df37796d2a696f7548eed2239e42fe3e0f63"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sorairolake/hf.git", branch: "develop"
 
