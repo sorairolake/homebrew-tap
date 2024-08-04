38190be4fedf8e6d5b49994e6367e660f51c8b22
Shun Sakai <sorairolake@protonmail.ch>
2024-08-04 20:02:45 +0900

    Update `rzopfli.rb` (#12)

--- Formula/rzopfli.rb
+++ Formula/rzopfli.rb
@@ -5,8 +5,8 @@
 class Rzopfli < Formula
   desc "Lossless data compression tool using Zopfli"
   homepage "https://sorairolake.github.io/rzopfli/"
-  url "https://github.com/sorairolake/rzopfli/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "b6d3bdae8fba5975515d1df4b0bc7e65aa68b243866dcbf695e0463448abd8aa"
+  url "https://github.com/sorairolake/rzopfli/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "d96f8a5691112512623ea5634afab129704472ce614f4416450184f6025ba118"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sorairolake/rzopfli.git", branch: "develop"
 
