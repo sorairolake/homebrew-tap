316c28fe7149a87a5d54954e9428a7b4eacea4de
Shun Sakai <sorairolake@protonmail.ch>
2025-07-28 21:00:00 +0900

    chore: Update `hf.rb`

--- Formula/hf.rb
+++ Formula/hf.rb
@@ -5,8 +5,8 @@
 class Hf < Formula
   desc "Utility for hidden files and directories"
   homepage "https://sorairolake.github.io/hf/"
-  url "https://github.com/sorairolake/hf/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "30b4475571c3191f8b7cc229da5d21490ae5fee4d67455818d8f7eee779d1a1a"
+  url "https://github.com/sorairolake/hf/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "3b2920e53ed5628b5ab44ab1400835a3ead1c0b7fe73722b1512a3ca4e41c6d9"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sorairolake/hf.git", branch: "develop"
 
@@ -16,8 +16,8 @@ class Hf < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    out_dir = Dir["target/release/build/hf-*/out"].first
-    man1.install Dir["#{out_dir}/*.1"]
+    system "asciidoctor", "-b", "manpage", "docs/man/man1/*.1.adoc"
+    man1.install Dir["docs/man/man1/*.1"]
 
     system "#{bin}/hf completion bash > hf.bash"
     system "#{bin}/hf completion fish > hf.fish"
