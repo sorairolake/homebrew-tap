bdd2dcd05845ad107441c2082ea8e84c0b7fb287
Shun Sakai <sorairolake@protonmail.ch>
2025-07-28 23:10:08 +0900

    chore: Update `ngrv.rb` (#50)

--- Formula/ngrv.rb
+++ Formula/ngrv.rb
@@ -5,8 +5,8 @@
 class Ngrv < Formula
   desc "Terminal-based pipe viewer similar to `pv(1)`"
   homepage "https://github.com/sorairolake/ngrv"
-  url "https://github.com/sorairolake/ngrv/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "652a1d1210c56307d6f2263c9b4c0068ad3138436b115455a9feddeb0eb8bbc8"
+  url "https://github.com/sorairolake/ngrv/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "a3561a747c9b0bd955b18d01efcfc31bb233c3abdea7db95213b01abdb438237"
   license "GPL-3.0-or-later"
   head "https://github.com/sorairolake/ngrv.git", branch: "develop"
 
@@ -16,8 +16,8 @@ class Ngrv < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    out_dir = Dir["target/release/build/ngrv-*/out"].first
-    man1.install Dir["#{out_dir}/*.1"]
+    system "asciidoctor", "-b", "manpage", "docs/man/man1/ngrv.1.adoc"
+    man1.install "docs/man/man1/ngrv.1"
 
     system "#{bin}/ngrv --generate-completion bash > ngrv.bash"
     system "#{bin}/ngrv --generate-completion fish > ngrv.fish"
