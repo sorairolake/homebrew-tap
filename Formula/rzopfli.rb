b01bfddfff1e3ce2e73efd9548c53b0bc6806750
Shun Sakai <sorairolake@protonmail.ch>
2025-07-28 23:02:51 +0900

    chore: Update `rzopfli.rb`

--- Formula/rzopfli.rb
+++ Formula/rzopfli.rb
@@ -5,8 +5,8 @@
 class Rzopfli < Formula
   desc "Lossless data compression tool using Zopfli"
   homepage "https://sorairolake.github.io/rzopfli/"
-  url "https://github.com/sorairolake/rzopfli/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "db0b8023126a46417ed4d8ac735a60aa92177886fbc105676370265b8c1e2752"
+  url "https://github.com/sorairolake/rzopfli/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "5ccf94507d1d98458015c136fec62bb543fa528fb2af8df37dbf85aafe2ff43a"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/sorairolake/rzopfli.git", branch: "develop"
 
@@ -16,8 +16,8 @@ class Rzopfli < Formula
   def install
     system "cargo", "install", *std_cargo_args
 
-    out_dir = Dir["target/release/build/rzopfli-*/out"].first
-    man1.install Dir["#{out_dir}/*.1"]
+    system "asciidoctor", "-b", "manpage", "docs/man/man1/rzopfli.1.adoc"
+    man1.install "docs/man/man1/rzopfli.1"
 
     system "#{bin}/rzopfli --generate-completion bash > rzopfli.bash"
     system "#{bin}/rzopfli --generate-completion fish > rzopfli.fish"
